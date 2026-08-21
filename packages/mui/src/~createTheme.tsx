7e781e849f7d13d9180bc17e8b8fb53cf0b786b7
Mark Dane <48362463+angrycat9000@users.noreply.github.com>
2026-08-21 10:08:44 -0400

    `ListSubheader` structure (#1744)
    Co-authored-by: Mayank <9084735+mayank99@users.noreply.github.com>

--- packages/mui/src/~createTheme.tsx
+++ packages/mui/src/~createTheme.tsx
@@ -454,7 +454,7 @@ function createTheme(args: CreateThemeArgs) {
 					},
 				},
 			},
-			MuiListSubheader: { defaultProps: { component: Role.li } },
+			MuiListSubheader: { defaultProps: { component: Role.div } },
 			MuiMenu: {
 				defaultProps: {
 					component: Role.div,
