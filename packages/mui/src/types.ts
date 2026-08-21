b7397d52f0d60236ca7690288868de0d57a365c0
Mark Dane <9084735+mayank99@users.noreply.github.com>
2026-08-21 13:46:06 -0400

    Deprecate `Tooltip` props (#1722)
    Co-authored-by: Gerardas B. <10091419+GerardasB@users.noreply.github.com>

--- packages/mui/src/types.ts
+++ packages/mui/src/types.ts
@@ -1106,13 +1106,31 @@ declare module "@mui/material/ToggleButtonGroup" {
 
 declare module "@mui/material/Tooltip" {
 	interface TooltipProps {
+		/** @deprecated StrataKit does not support this prop. */
+		arrow?: TooltipProps["arrow"];
 		/**
 		 * The default value with `@stratakit/mui` is `true`.
 		 * Use `describeChild={false}` if you want to label the child element.
 		 *
 		 * @default true
 		 */
-		describeChild?: boolean;
+		describeChild?: TooltipProps["describeChild"];
+		/** @deprecated StrataKit does not support this prop. */
+		disableFocusListener?: TooltipProps["disableFocusListener"];
+		/** @deprecated StrataKit does not support this prop. */
+		disableHoverListener?: TooltipProps["disableHoverListener"];
+		/** @deprecated StrataKit does not support this prop. */
+		disableInteractive?: TooltipProps["disableInteractive"];
+		/** @deprecated StrataKit does not support this prop.  */
+		disableTouchListener?: TooltipProps["disableTouchListener"];
+		/** @deprecated StrataKit does not support this prop. */
+		enterDelay?: TooltipProps["enterDelay"];
+		/** @deprecated StrataKit does not support this prop. */
+		enterNextDelay?: TooltipProps["enterNextDelay"];
+		/** @deprecated StrataKit does not support this prop. */
+		enterTouchDelay?: TooltipProps["enterTouchDelay"];
+		/** @deprecated StrataKit does not support this prop. */
+		followCursor?: TooltipProps["followCursor"];
 	}
 }
 
